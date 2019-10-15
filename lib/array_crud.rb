4d15cdb48dda9b20556ee2dbb6d52368dfa6e5e9
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-10-15 17:15:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,8 +24,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = []
-  index_number = ()
+ 
   return array[index_number]
 end
 
